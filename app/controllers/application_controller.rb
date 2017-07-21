6382538768cf5b3cb1c60d8fe99b483f60392ba7
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-21 10:30:21 -0700

    Add end point GET by id and exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_response({ message: exception.message}, :not_found)
+  end
 end
--- app/controllers/local_business_controller.rb
+++ app/controllers/local_business_controller.rb
@@ -5,8 +5,39 @@ class LocalBusinessController < ApplicationController
     json_response(@local_business)
   end
 
-  private
-  def json_response(object)
-    render json: object, status: :ok
+  def show
+    @local_businesses = LocalBusiness.find(params[:id])
+    json_response(@local_businesses)
   end
+
+  def create
+    @local_businesses = LocalBusiness.create!(local_businesses_params)
+    json_response(@local_businesses)
+
+  end
+
+  def update
+    @local_businesses = LocalBusiness.find(params[:id])
+    if @local_businesses.update!(local_businesses_params)
+      render status: 200, json: {
+        message: "Local Business updated"
+      }
+      end
+    end
+
+  def destroy
+    @local_businesses = LocalBusiness.find(params[:id])
+    if @local_businesses.destroy!
+      render status: 200, json: {
+        message: "Local Business deleted"
+      }
+    end
+  end
+
+
+private
+  def local_business_params
+    params.permit(  :name,:street, :city, :state, :country, :zipcode, :phone_number, :business_type, :current_status, :key_peoples, :founder, :founded, :line_of_business, :number_of_employees,  :website)
+  end
+
 end
